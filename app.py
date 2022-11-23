b67509d8abbd3b1e8b978d2799961e06e556f859
nou-man <4pa20cs059@pace.edu.in>
2022-11-23 15:57:34 +0530

    progress

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def main():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
